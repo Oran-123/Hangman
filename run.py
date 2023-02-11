0e403b8323fb3c413ea1c656f1a3e1246f6dcdbb
Oran-123 <orandillon123@gmail.com>
2023-02-11 12:48:20 +0000

    add to function to ask for username

--- run.py
+++ run.py
@@ -73,51 +73,64 @@ def play_hangman():
     lives = 7 
     attempts = 0 
     
-    # let user select letters until word is guessed correctly 
-    while len(answer_letters) > 0 and lives > 0: 
+    #ask for username 
+    username_valid_response = False
 
-        #display art 
-        print(HANGMAN_STAGES[attempts])
-        #display current lives 
-        print(lives)
+    while username_valid_response == False:
+        username = input("Enter your name:")
+        if len(username) > 0:
+            username_valid_response = True
+        else:
+            username_valid_response = False
+    
 
-        #display the letters the user selected 
-        print(f'Used Letters: {used_letters}')
+    while username_valid_response == True:
+        # let user select letters until word is guessed correctly 
+        while len(answer_letters) > 0 and lives > 0: 
 
-        #dispaly guessed words
-        print(f'Used Words: {used_words}')
+            #display art 
+            print(HANGMAN_STAGES[attempts])
+            #display current lives 
+            print(f'{username} you have {lives} remaining')
 
-        #display the hidden word
-        hidden_answer_letters = [letter if letter in used_letters else "_" for letter in answer]
-        print("Current Word:" ," ".join(hidden_answer_letters) )
+            #display the letters the user selected 
+            print(f'Used Letters: {used_letters}')
 
-        #user input, and validation 
-        selected_letter = input("Guess a letter: ").upper()
-        if len(selected_letter) == 1 and selected_letter.isalpha():
-            if selected_letter in used_letters:
-                print(f"Oops... you have already selected {selected_letter}, try typing a different letter!")
-            elif selected_letter not in used_letters:
-                used_letters.append(selected_letter)
-                if selected_letter in answer_letters:
-                    available_letters.remove(selected_letter)
-                    print("Well done, that is correct!")
-                elif selected_letter not in answer_letters:
-                    print("Oh no, that is incorrect!")
-                    lives -= 1
-                    attempts += 1  
-        elif len(selected_letter) == len(answer) and selected_letter.isalpha():
-            if selected_letter in used_letters:
-                print(f"Oops... you have already selected {selected_letter}, try typing a different letter!")
-            elif selected_letter not in used_letters:
-                used_letters.append(selected_letter)
-                if selected_letter == answer:
-                    print("Well done, that is correct!")
-                elif selected_letter != answer:
-                    used_words.append(selected_letter)
-                    print("Oh no, that is incorrect!")
-                    lives -= 1
-                    attempts += 1  
-        else: 
-            print("Invalid, character. Please try typing a letter!")     
-       
+            #dispaly guessed words
+            print(f'Used Words: {used_words}')
+
+            #display the hidden word
+            hidden_answer_letters = [letter if letter in used_letters else "_" for letter in answer]
+            print("Current Word:" ," ".join(hidden_answer_letters) )
+
+            #user input, and validation 
+            selected_letter = input("Guess a letter: ").upper()
+            if len(selected_letter) == 1 and selected_letter.isalpha():
+                if selected_letter in used_letters:
+                    print(f"Oops... you have already selected {selected_letter}, try typing a different letter!")
+                elif selected_letter not in used_letters:
+                    used_letters.append(selected_letter)
+                    if selected_letter in answer_letters:
+                        available_letters.remove(selected_letter)
+                        print("Well done, that is correct!")
+                    elif selected_letter not in answer_letters:
+                        print("Oh no, that is incorrect!")
+                        lives -= 1
+                        attempts += 1  
+            elif len(selected_letter) == len(answer) and selected_letter.isalpha():
+                if selected_letter in used_letters:
+                    print(f"Oops... you have already selected {selected_letter}, try typing a different letter!")
+                elif selected_letter not in used_letters:
+                    used_letters.append(selected_letter)
+                    if selected_letter == answer:
+                        print("Well done, that is correct!")
+                    elif selected_letter != answer:
+                        used_words.append(selected_letter)
+                        print("Oh no, that is incorrect!")
+                        lives -= 1
+                        attempts += 1  
+            else: 
+                print("Invalid, character. Please try typing a letter!")     
+
+
 playgame_or_leaderboard()
